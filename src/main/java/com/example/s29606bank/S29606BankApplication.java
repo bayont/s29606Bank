193ed85691ca5339282479f735f8dbe555804653
fabian.fetter <fabian.fetter@woodpecker.co>
2024-01-27 14:53:46 +0100

    Initial commit

---
+++ src/main/java/com/example/s29606bank/S29606BankApplication.java
@@ -0,0 +1,13 @@
+package com.example.s29606bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S29606BankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S29606BankApplication.class, args);
+    }
+
+}
